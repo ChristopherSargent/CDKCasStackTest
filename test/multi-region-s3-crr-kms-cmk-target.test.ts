1f0198a56173320a8033389a64426d74c042e0b0
Christopher Sargent <christopher.sargent@sargentwalker.io>
2023-07-21 16:17:38 +0000

    Initial commit

---
+++ test/multi-region-s3-crr-kms-cmk-target.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MultiRegionS3CrrKmsCmkTarget from '../lib/multi-region-s3-crr-kms-cmk-target-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/multi-region-s3-crr-kms-cmk-target-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MultiRegionS3CrrKmsCmkTarget.MultiRegionS3CrrKmsCmkTargetStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
